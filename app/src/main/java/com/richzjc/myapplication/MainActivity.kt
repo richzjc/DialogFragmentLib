e3475aa0e5d2c67f7e90309d7aef8c9fc6d281c2
richzjc <zhangjianchuan@wallstreetcn.com>
2019-12-02 16:35:32 +0800

    add files

--- app/src/main/java/com/richzjc/myapplication/MainActivity.kt
+++ app/src/main/java/com/richzjc/myapplication/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val dialog = TestDialog()
-        dialog.show(supportFragmentManager, "testDialog")
+//        val dialog = TestDialog()
+//        dialog.show(supportFragmentManager, "testDialog")
     }
 }
